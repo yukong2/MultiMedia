5b6ea4e2074dd1116ff3a9405091ee4397431786
yukong2 <rladbqls0212@gmail.com>
2024-11-17 18:49:44 +0900

    초기

---
+++ Assets/Undead Survivor/Codes/Reposition.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area"))
+            return;
+
+        Vector3 playerPos = GameManager.Instance.player.transform.position;
+        Vector3 myPos = transform.position;
+
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
+
+        float diffX = Mathf.Abs(dirX);
+        float diffY = Mathf.Abs(dirY);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if(diffX > diffY)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if (diffX < diffY)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+            case "Enemy":
+                break;
+        }
+    }
+}
